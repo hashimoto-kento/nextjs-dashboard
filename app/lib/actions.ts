3988b71c639d828c02c5dd9f254cbb67095ea3b7
DESKTOP-J40HMMS\Owner <bug.bee.ron@gmail.com>
2024-10-25 08:40:13 +0900

    ESLint adoption

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -56,7 +56,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
   INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amounInCents}, ${status}, ${date})
   `;
-} catch (error) {
+} catch {
   return {
     message: 'Database Error: Failed to Create Invoice',
   };
@@ -93,7 +93,7 @@ export async function updateInvoice(
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
-  } catch (error) {
+  } catch {
     return {
       message: 'Database Error: Failed to Update Invoice',
     };
@@ -107,7 +107,7 @@ export async function deleteInvoice(id: string) {
   try {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
-  } catch (error) {
+  } catch {
     return {
       message: 'Database Error: Failed to Delete Invoice',
     };
